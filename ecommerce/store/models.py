ca006ee3d6312d0f79a87449aab148f61f00e35a
meryem <116671202+meryemorc@users.noreply.github.com>
2025-05-20 19:52:08 +0300

    product models add category

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -18,6 +18,8 @@ class Category(models.Model):
         return self.name 
     
 class Product(models.Model):
+
+    category = models.ForeignKey(Category , related_name='product' , on_delete= models.CASCADE , null ='True')
     
     title = models.CharField(max_length=250)
 
